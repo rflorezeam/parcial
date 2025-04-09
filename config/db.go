47b932b7d6414aaa9ba7b08b4ed1500ebf3590f1
Ricardo <florez.ricardo.2784@eam.edu.co>
2025-04-09 01:01:02 -0500

    listos los tests y su cobertura

--- config/db.go
+++ config/db.go
@@ -13,7 +13,16 @@ import (
 var DB *mongo.Database
 
 func ConectarDB() {
-	mongoURI := os.Getenv("MONGO_URI")
+	env := os.Getenv("ENV")
+	var mongoURI string
+
+	if env == "dev" {
+		mongoURI = os.Getenv("MONGO_URI_TEST")
+		fmt.Println("🧪 Entorno de desarrollo: conectando a Mongo LOCAL")
+	} else {
+		mongoURI = os.Getenv("MONGO_URI")
+		fmt.Println("🚀 Entorno de producción: conectando a Mongo DOCKER")
+	}
 	dbName := os.Getenv("MONGO_DB")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
